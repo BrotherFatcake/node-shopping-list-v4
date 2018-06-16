91a3ce397a736864d6f2c743b319b297c83534c8
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-06-16 18:17:47 -0500

    add put

--- server.js
+++ server.js
@@ -76,15 +76,14 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   });
   res.status(204).end();
 });
-
 // when DELETE request comes in with an id in path,
 // try to delete that item from ShoppingList.
 app.delete('/shopping-list/:id', (req, res) => {
   ShoppingList.delete(req.params.id);
+        
   console.log(`Deleted shopping list item \`${req.params.ID}\``);
   res.status(204).end();
 });
-
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
@@ -108,6 +107,31 @@ app.post('/recipes', jsonParser, (req, res) => {
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
   console.log(`Deleted recipe \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+app.put('/recipes/:id', jsonParser, (req, res)  =>  {
+  const fieldCheck = ['name', 'ingredients', 'id'];
+  for (let i=0; i<fieldCheck.length; i++ ) {
+    const field = fieldCheck[i];
+    if (!(field in req.body)) {
+      const message = `missing \'${field}\' in request`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  
+  if (req.params.id !== req.body.id)  {
+    const message = `request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message)
+    return res.status(400).send(message);
+  }
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
   res.status(204).end();
 });
 
